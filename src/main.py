2d87a13f83d207a6ed0df7de6de7aea619158377
Douglas Luman <dluman@allegheny.edu>
2026-03-27 13:36:11 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,14 +4,13 @@ from Sensor import Sensor
 from Thermometer import Thermometer
 
 def main():
-    sensor = Sensor()
-    thermo = Thermometer()
-    thermo.reset()
-    power_off = Pin(15, Pin.IN, Pin.PULL_UP)
-    while power_off.value() != 0:
-        voltage = sensor.read_sensor_voltage()
-        thermo.calculate_temps(voltage)
-    thermo.reset()
+    """
+    Main function to run the thermometer program.
+
+    Function initializes the sensor and thermometer, and continuously reads the temperature until the power off button is pressed.
+    """
+    # TODO
+    pass
 
 if __name__ == "__main__":
     main()
