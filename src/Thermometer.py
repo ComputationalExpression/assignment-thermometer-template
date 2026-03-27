24f7a134f47dfdc9012e66c913e8c59c77f5ddfd
Douglas Luman <dluman@allegheny.edu>
2026-03-27 13:35:39 -0400

    Update Thermometer.py

--- src/Thermometer.py
+++ src/Thermometer.py
@@ -11,34 +11,35 @@ class Thermometer:
         }
 
     def reset(self) -> None:
-        for light in self.lights:
-            self.lights[light].on()
-        sleep(1)
-        self.lights_out()
+        """
+        Reset the thermometer's lights sequence.
+        """
+        # TODO
+        pass
 
     def lights_out(self) -> None:
-        for light in self.lights:
-            self.lights[light].off()
+        """
+        Turn all lights in self.lights off.
+        """
+        # TODO
+        pass
 
     def set_led_output(self) -> None:
-        # Turn all lights off
-        self.lights_out()
-        # We'll use F after all
-        led = None
-        if self.temp_in_f <= 32:
-            led = "blue"
-        elif 32 < self.temp_in_f <= 70:
-            led = "yellow"
-        else:
-            led = "red"
-        self.lights[led].on()
-
-    def calculate_temps(self, voltage: float) -> None:
-        self.convert_temp_to_c(voltage)
-        self.convert_temp_to_f()
+        """
+        Set the LED output based on the current temperature in Fahrenheit.
+        """
+        # TODO
+        pass
 
     def convert_temp_to_c(self, voltage: float) -> None:
-        self.temp_in_c = 27 - (voltage - 0.706) / 0.001721
+        """
+        Convert the voltage reading from the temperature sensor to Celsius and store it in self.temp_in_c."""
+        # TODO
+        pass
 
     def convert_temp_to_f(self) -> None:
-        self.temp_in_f = (self.temp_in_c * 9/5) + 32
+        """
+        Convert the temperature in Celsius to Fahrenheit and store it in self.temp_in_f.
+        """
+        # TODO
+        pass
